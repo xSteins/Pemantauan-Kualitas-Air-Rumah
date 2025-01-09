a5185e3cf2ec12c84e451d50321669dd7e5ef51f
Ralvin Adrian <itsmesteins@gmail.com>
2025-01-09 17:57:14 +0700

    kalibrasi sensor ph & temperatur

--- Arduino-ESP-Code/Sensor_Reading_Mega_2560/Sensor_Reading_Mega_2560.ino
+++ Arduino-ESP-Code/Sensor_Reading_Mega_2560/Sensor_Reading_Mega_2560.ino
@@ -1,21 +1,18 @@
-// #include <DallasTemperature.h>
-// #include <OneWire.h>
+#include <DallasTemperature.h>
+#include <OneWire.h>
 #include <GravityTDS.h>
 
 GravityTDS gravityTds;
 
 // pinout untuk masing-masing sensor
-// A0 Po (PH)
-// A1 To (Temp-Ph : Temp)
-// A2 Tur (Turbidity : Percentage)
-// A3 Tds (Tds : PPM)
 const int AnalogPh = A0;
-const int AnalogTemp = A1;         
-const int AnalogTurb = A2;   
-const int AnalogTDS = A3;        
+const int AnalogTurb = A1;   
+const int AnalogTDS = A2;        
+// pinout digital u/ temperatur
+#define ONE_WIRE_BUS 24
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire); 
 
-// OneWire oneWire(AnalogTe);
-// DallasTemperature sensors(&oneWire); 
 
 void setup(){
   // baud rate maksimum u/ connect dgn esp8266
@@ -27,61 +24,82 @@ void setup(){
   gravityTds.setAdcRange(1024); // resolusi data
   gravityTds.begin(); // init
 
-  // set-up Ph
-  pinMode(AnalogPh,INPUT);
+  // init sensor temperatur
+  sensors.begin();
+  // untuk debugging onewire
+  // int numberOfDevices = sensors.getDeviceCount();
+  // Serial.print(numberOfDevices, DEC);
+  // Serial.println(" devices.");
 }
 
+
 void loop(){
-  // Serial.print("PH: ");
-  // Serial.print(get_ph_reading());
-  // Serial.print(" Temp: ");
-  // Serial.print(get_temp_reading());
-  // Serial.print(" TDS: ");
-  // Serial.print(get_tds_reading());
-  // Serial.print(" TUR: ");
-  // Serial.println(get_turb_reading());
-  Serial.print(get_ph_reading());
+  // Untuk kirim data ke ESP pakai # sebagai separator value
+  get_ph_reading();
   Serial.print("#");
   Serial.print(get_temp_reading());
   Serial.print("#");
-  Serial.print(get_tds_reading());
+  get_tds_reading();
   Serial.print("#");
-  Serial.println(get_turb_reading());
+  get_turb_reading();
+  Serial.println();
   delay(5000);
 }
 
 // https://cimpleo.com/blog/arduino-ph-meter-using-ph-4502c/
 // kalibrasi : https://raaflahar.medium.com/ph-4502c-sensor-diymore-how-to-use-and-calibrate-using-arduino-uno-r3-3afc2b96631
-float phCalibration4 = 3.1; 
-float phCalibration7 = 16.6;
-float get_ph_reading(){
-  int analog_ph_reading = analogRead(AnalogPh); // deklarasikan analog_ph_reading
-  float TeganganPh = 5 / 1024.0 * analog_ph_reading; // deklarasikan TeganganPh
-
+float valPh4 = 3.50;
+float valPh7 = 3.00;
+void get_ph_reading(){
+  // pinout analog (P0 di PCB)
+  int analog_ph_reading = analogRead(AnalogPh);
+  // hitung kompensasi voltase dgn reading analognya
+  float TeganganPh = 5 / 1024.0 * analog_ph_reading; 
+  // Serial.print(" ADC pH : ");
+  // Serial.println(TeganganPh);
   // hitung nilai pH menggunakan tegangan yang diukur
-  float pH = 7.0 + ((2.5 - TeganganPh) / (phCalibration7 - phCalibration4)) * (7.0 - 4.0);
-  return pH;
+  float pH_Step = (valPh4 - valPh7) / 3;
+  float pH = 7.0 + ((valPh7-TeganganPh) / pH_Step);
+  Serial.print(pH);
 }
 
 float get_temp_reading(){
-  // implementasi sensor 
-  float analog_temp = analogRead(AnalogTemp);
-  float voltage = analog_temp * (5.0 / 1024.0);
-  return voltage;
+  // request temperatur
+  sensors.requestTemperatures();
+  // return temperatur dalam bentuk celsius
+  return sensors.getTempCByIndex(0);
 }
 // https://wiki.dfrobot.com/Turbidity_sensor_SKU__SEN0189
-float get_turb_reading(){
+void get_turb_reading(){
+  int analog_turb_reading = analogRead(AnalogTurb);
+  // validasi jika sensor gagal
+  if (analog_turb_reading <= 0) { 
+    Serial.print("-503");
+    return;
+  }
+
   // pakai mode analog
   // rumus : baca pin * (voltase device (3.3 esp, 5.0 ardduino) / 1024.0)
   // voltage = analogRead(pin) * (5.0 / 1024.0)
   // karena mau baca persentase, anggap saja
-  return 100.0 - analogRead(AnalogTurb) * (5.0 / 1024.0);
+  float turb_reading = 100.0 - analogRead(AnalogTurb) * (5.0 / 1024.0);
+  Serial.print(turb_reading);
 }
 
-float get_tds_reading(){
-  // gravityTds.setTemperature(get_temp_reading());
-  // gravityTds.setTemperature(24.0);
+void get_tds_reading(){
+  float temp = get_temp_reading();
+  if (temp == -503) { 
+    // set error code juga kalo suhu tidak bisa dibaca
+    Serial.print("-503");
+    return;
+  }
+  gravityTds.setTemperature(temp);
   gravityTds.update();
+
   float tdsValue = gravityTds.getTdsValue();
-  return tdsValue;
+  if (tdsValue <= 0) { // validasi jika TDS gagal baca
+    Serial.print("-503");
+    return;
+  }
+  Serial.print(tdsValue);
 }
